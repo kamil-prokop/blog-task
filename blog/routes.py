ed18a36d064bfc304fd9ae836c991329a3e1e3af
Kamil Prokop <zara.pug.yt@gmail.com>
2023-04-01 23:57:38 +0200

    adding blog posts

--- blog/routes.py
+++ blog/routes.py
@@ -1,6 +1,26 @@
 from flask import render_template
 from blog import app
+from blog.models import Entry
 
 @app.route("/")
 def index():
-   return render_template("base.html")
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+
+    return render_template("homepage.html", all_posts=all_posts)
+
+    
+
+from faker import Faker
+from blog.models import Entry, db
+
+def generate_entries(how_many=10):
+   fake = Faker()
+
+   for i in range(how_many):
+       post = Entry(
+           title=fake.sentence(),
+           body='\n'.join(fake.paragraphs(15)),
+           is_published=True
+       )
+       db.session.add(post)
+   db.session.commit()
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-import os
+import os.path
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))    
 
